c12801a11ed63146b46d8a93f84467597474566e
naijamesz <kodlikejamesz@hotmail.com>
2024-02-04 07:46:13 +0700

    Added json-server & axios CRUD api.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 // BookList component = child component of App component.
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 export default function App() {
   const [books, setBooks] = useState([]);
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
 
+    setBooks(response.data);
+  };
+
+  // DONT DO THIS: Because it will cause an infinite loop
+  // fetchBooks();
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
   // Function to update the books array when a book is edited
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle
+    });
+    console.log(response.data);
     const updatedBooks = books.map(book => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
       return book;
@@ -19,7 +35,9 @@ export default function App() {
     setBooks(updatedBooks);
   };
   // Function to delete the books by id in array when a book is edited
-  const deleteBookById = id => {
+  const deleteBookById = async id => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter(book => {
       return book.id !== id;
     });
@@ -27,14 +45,10 @@ export default function App() {
     setBooks(updatedBooks);
   };
   // Function to create a new book with a random id and the provided title
-  const createBook = title => {
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ];
+
+  const createBook = async title => {
+    const response = await axios.post('http://localhost:3001/books', { title });
+    const updatedBooks = [...books, response.data];
     // Update the state with the new books array
     setBooks(updatedBooks);
   };
@@ -49,4 +63,3 @@ export default function App() {
     </>
   );
 }
-
